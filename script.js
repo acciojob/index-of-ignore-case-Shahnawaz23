8a9e6b33f896b9e98ba879a31bdaf32a0e79b870
Shahnawaz Akhtar <58129001+Shahnawaz23@users.noreply.github.com>
2024-12-18 00:17:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	 if (!subStr) return 0; .
+	 if (!subStr) return 0; 
     return str.toLowerCase().indexOf(subStr.toLowerCase());
 }
 
